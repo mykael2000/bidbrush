4bc89c714f09bf54042a84b3c0245d92574a23a3
Michael <eramehmichael2000@gmail.com>
2025-05-26 23:56:36 +0100

    update

--- bidbrush-backend/routes/web.php
+++ bidbrush-backend/routes/web.php
@@ -19,12 +19,13 @@ Route::get('/account/db-test', function () {
     }
 });
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return 'Homepage OK';
+    // return Inertia::render('Welcome', [
+    //     'canLogin' => Route::has('login'),
+    //     'canRegister' => Route::has('register'),
+    //     'laravelVersion' => Application::VERSION,
+    //     'phpVersion' => PHP_VERSION,
+    // ]);
 });
 
 Route::get('/dashboard', function () {
