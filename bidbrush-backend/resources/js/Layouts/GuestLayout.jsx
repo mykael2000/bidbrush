50ad6cd768339051d83e29bdd1b33b6ed10df05e
Michael <eramehmichael2000@gmail.com>
2025-05-27 00:33:26 +0100

    update

--- bidbrush-backend/resources/js/Layouts/GuestLayout.jsx
+++ bidbrush-backend/resources/js/Layouts/GuestLayout.jsx
@@ -10,9 +10,9 @@ export default function GuestLayout({ children }) {
                 </Link>
             </div>
 
-            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
+            {/* <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
                 {children}
-            </div>
+            </div> */}
         </div>
     );
 }
