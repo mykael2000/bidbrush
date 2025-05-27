5b35526ee729a67e513f7f33c462c2dd5456993e
Michael <eramehmichael2000@gmail.com>
2025-05-27 01:21:56 +0100

    update

--- bidbrush-backend/resources/js/Layouts/GuestLayout.jsx
+++ bidbrush-backend/resources/js/Layouts/GuestLayout.jsx
@@ -6,13 +6,13 @@ export default function GuestLayout({ children }) {
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
                 <Link href="/">
-                    <img className="h-20 w-20 fill-current text-gray-500" src="bid.jpeg" alt='logo' />
+                    <img className="h-20 w-20 fill-current text-gray-500" src={bid} alt='logo' />
                 </Link>
             </div>
 
-            {/* <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
+            <div className="mt-6 w-full overflow-hidden bg-white px-6 py-4 shadow-md sm:max-w-md sm:rounded-lg dark:bg-gray-800">
                 {children}
-            </div> */}
+            </div>
         </div>
     );
 }
