24e642ac6bee273426f5e6579435193ba774d51d
Michael <eramehmichael2000@gmail.com>
2025-05-27 01:11:11 +0100

    upload

--- bidbrush-backend/resources/js/Layouts/GuestLayout.jsx
+++ bidbrush-backend/resources/js/Layouts/GuestLayout.jsx
@@ -1,12 +1,12 @@
 import ApplicationLogo from '@/Components/ApplicationLogo';
 import { Link } from '@inertiajs/react';
-
+import bid from '../images/bid.jpeg';
 export default function GuestLayout({ children }) {
     return (
         <div className="flex min-h-screen flex-col items-center bg-gray-100 pt-6 sm:justify-center sm:pt-0 dark:bg-gray-900">
             <div>
                 <Link href="/">
-                    <img className="h-20 w-20 fill-current text-gray-500" href="https://bidbrush.com/bid.jpeg" />cggchg
+                    <img className="h-20 w-20 fill-current text-gray-500" src="bid.jpeg" alt='logo' />
                 </Link>
             </div>
 
